e17e7e577111ac153eec53075468d6d3eb0b03d0
IsraelChaves <165285837+IsraelChaves@users.noreply.github.com>
2024-09-03 13:56:45 -0300

    Add files via upload

---
+++ source.js
@@ -0,0 +1,13 @@
+var title = document.getElementById("titulo-principal")
+
+function titulo(){
+    if (title.innerText == "Nenem Está"){
+        title.innerText = "Nenem Está Casada"
+    }else{
+        title.innerText = "Nenem Está"
+    }
+}
+
+function hello() {
+    console.log("Hello World")
+}
